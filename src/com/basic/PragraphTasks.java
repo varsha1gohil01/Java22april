ce12d99a4abba87e6da1a38ac4a6ceec8fbdf3ae
varsha1gohil01 <varshabaraiya25@gmail.com>
2025-08-19 19:30:24 +0530

    paragraph tasks 19 -8 2025

---
+++ src/com/basic/PragraphTasks.java
@@ -0,0 +1,65 @@
+package com.basic;
+
+public class PragraphTasks {
+	public static void main(String[] args) {
+		
+		String str1 = "This is a sample paragraph.\nIt consists of multiple lines.\n"+"Each line will be displayed separately.";
+		
+		System.out.println("Display paragraph using one variable =\n"+str1+"\n");
+		
+		String str2 = "This is a paragraph about Tops.\n"+ "Tops is 12 years old and lives in Surat.\n"+ "He enjoys coding in his free time.\n";
+		
+		System.out.println("Display paragraph using variable =\n"+str2);
+		
+		String str3 = "Myself Harish Thirani, I hail from the city of Secunderabad which is \nlocated in Hyderabad, as Telangana district.\n"
+				+ "I recently graduated from Osmania University with a Bachelor of \nCommerce (Computer Application).\n"
+				+ "Well, I am looking forward to commencing my career in this role. I am \\nseeking an entry-level position to uplift my\\n skills and strengthen my experience.\n"
+				+ "As I am committed to perpetual learning, I will contribute positively to \\nthe organization.\n"
+				+ "And have a desire to work in a structured environment well, I am \nexcited to begin my path of success and take on new challenges.\n"
+				+ "Thank you.\r\n"
+				+ "";
+		
+		
+		System.out.println("Display paragraph using variable =\n"+str3);
+		
+		System.out.println("display a paragraph without variable using println =\n");
+		 
+		
+		    System.out.println("This is a paragraph about Tops.");
+	        System.out.println("Tops is 12 years old and lives in Surat.");
+	        System.out.println("He enjoys coding in his free time.");
+
+	        System.out.println();
+	        
+	        System.out.println("display a paragraph without variable using print =\n");
+	       
+	        System.out.print("This is a paragraph about Tops.\n");
+	        System.out.print("Tops is 12 years old and lives in Surat.\n");
+	        System.out.print("He enjoys coding in his free time.\n");
+	
+	        
+	        
+	        
+	        System.out.println("\nprint a person's full name using only character variables =\n");
+	        
+	        char c1 = 'V';
+	        char c2 = 'A';
+	        char c3 = 'R';
+	        char c4 = 'S';
+	        char c5 = 'H';
+	        char c6 = 'A';
+
+	        
+	      
+	        char c7 = 'G';
+	        char c8 = 'O';
+	        char c9 = 'H';
+	        char c10 = 'I';
+	        char c11 = 'L';
+	      
+	        // Print full name by concatenating chars
+	        System.out.println("" + c1 + c2 + c3 + c4 + c5 + c6 +" "  + c7+c8+c9+c10+c11);
+	        
+	}
+
+}
--- src/com/basic/TypeCasting.java
+++ src/com/basic/TypeCasting.java
@@ -25,8 +25,24 @@ public class TypeCasting {
 		 int i2 = (int)l1;
 		 System.out.println("explicite conversion= "+i2);
 		 
+		 int i3 = 128; // 4 byte
+		 byte b1 = (byte)i3; // 1 byte // - 128 -127 - 126 -125 -124 -123 to 127
 		 
+		 System.out.println("explicite conversion= "+b1);
+		 
+		 
+		 
+		char cha = 'a';//2 byte
+		int i4 = cha; // 4 byte
 		
+		// it will print ascii value 
+		 System.out.println("implicite conversion= "+i4);
+		 
+		float f1 = 41.5f;
+		int i5 = (int)f1;
+		 System.out.println("implicite conversion= "+i5);
+		 
+	
 		
 	}
 }
